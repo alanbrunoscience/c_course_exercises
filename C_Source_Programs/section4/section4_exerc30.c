05ce5b80c1467b66a42c205e5f49166a45518426
Alan Bruno <alanbrunomath@gmail.com>
2023-09-08 18:26:48 -0300

    Update solutions

--- C_Source_Programs/section4/section4_exerc30.c
+++ C_Source_Programs/section4/section4_exerc30.c
@@ -2,9 +2,32 @@
 
 int main(){
 
-    int n1, n2, n3, vetNum[3];
+    int vetNum[3], minor, index = 0, temp;
 
-    // Pending    
+    for(int i = 0; i < 3; i++) {
+        printf("Enter the %dº number, please:\n", (i+1));
+        scanf("%d", &vetNum[i]);
+    }
+
+    for(int i = 0; i < 3; i ++){
+        minor = i;
+        for(int j = minor + 1; j <= 3; j++) {
+            if(vetNum[j] < vetNum[minor]){
+                minor = j;
+            }
+        }
+        temp = vetNum[i];
+        vetNum[i] = vetNum[minor];
+        vetNum[minor] = temp;
+    }
+
+    printf("|");
+
+    for(int k = 0; k < 3; k++) {
+        printf(" %d | ", vetNum[k]);
+    }
+
+    printf("\n");
 
     return 0;
 
