0492e89af6d20ca2bc899049572564a8dd85f5b7
Alan Bruno <alanbrunomath@gmail.com>
2023-09-19 22:22:42 -0300

    Update solutions

---
+++ C_Source_Programs/section5/section5_exerc28.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+
+    int num, fat;
+    float e = 0;
+
+    printf("Enter an integer number greater than 0, please:\n");
+    scanf("%d", &num);
+
+    if(num <= 0) {
+        printf("The number is invalid. It needs to be greater than zero.\n");
+    } else {
+        printf("E = 1");
+
+        for(int i = 1; i <= num; i++) {
+            fat = 1;
+            for(int j = i; j > 0; j--) {
+                fat *= j;
+            }
+            e += (1/(float)fat);
+            printf(" + 1/%d", fat);
+        }
+        printf(" = %.2f\n", e);
+    }
+
+    return 0;
+
+}
