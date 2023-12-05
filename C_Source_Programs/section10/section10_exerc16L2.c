2318b626e5e595054d9db64b7c4650c01b99a620
Alan Bruno <alanbrunomath@gmail.com>
2023-12-04 22:15:42 -0300

    Update solutions

--- C_Source_Programs/section10/section10_exerc16L2.c
+++ C_Source_Programs/section10/section10_exerc16L2.c
@@ -14,8 +14,8 @@ int main() {
 
     printf("\n*** RESULTS ***\n\n");
     printf("%d * 2 = %d;\n", *b, *b * 2);
-    printf("%d * 3 = %d\n", **c, **c * 3);
-    printf("%d * 4 = %d\n", ***d, ***d * 4);
+    printf("%d * 3 = %d;\n", **c, **c * 3);
+    printf("%d * 4 = %d.\n", ***d, ***d * 4);
 
     return 0;
 }
