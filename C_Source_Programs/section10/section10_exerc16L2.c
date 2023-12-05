f9e417f15b9f38dfcf6a2153d82b3968db19b4b6
Alan Bruno <alanbrunomath@gmail.com>
2023-12-04 22:14:59 -0300

    Update solutions

---
+++ C_Source_Programs/section10/section10_exerc16L2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int a, *b, **c, ***d;
+
+    printf("Enter an integer value, please: ");
+    scanf("%d", &a);
+
+    b = &a;
+    c = &b;
+    d = &c;
+
+    printf("\n*** RESULTS ***\n\n");
+    printf("%d * 2 = %d;\n", *b, *b * 2);
+    printf("%d * 3 = %d\n", **c, **c * 3);
+    printf("%d * 4 = %d\n", ***d, ***d * 4);
+
+    return 0;
+}
